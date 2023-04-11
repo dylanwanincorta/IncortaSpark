5fcbcf8009f59339aa05db0a401ebb4d692b6a24
Dylan Wan <dylan.wan@incorta.com>
2023-04-11 14:07:52 -0700

    Initial checkin

---
+++ __init__.py
@@ -0,0 +1,9 @@
+## The Python interpreter recognizes a folder as the package if it contains __init__.py file.
+## IncortaSpark becomes a package
+## An empty __init__.py file makes all functions 
+##     from the python code in this folder available when this package is imported.
+## The __init__.py file is normally kept empty. However, it can also be used to 
+##     choose specific functions from modules in the package folder and 
+##     make them available for import.
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+    name='IncortaSpark',
+    version='0.0.1',
+    description='Testing Python Package for Spark',
+    url='#',
+    author='Dylan Wan',
+    author_email='dylan.wan@incorta.com',
+    license='MIT',
+    packages=['util'],
+    zip_safe=False
+    )
